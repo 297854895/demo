850da4b7d5d2ec852efd7d068af129fcf3d7b99b
junar <4945040@qq.com>
2017-11-17 11:40:22 +0800

    33
    333

--- js/map.js
+++ js/map.js
@@ -1,30 +1,30 @@
 $(function(){
-  $(':input').labelauty();
+    $(':input').labelauty();
 });
 layui.use(['form', 'layedit', 'laydate', 'table'], function(){
-  var form = layui.form
-    ,layer = layui.layer
-    ,layedit = layui.layedit
-    ,laydate = layui.laydate;
-  //日期
-  laydate.render({
-    elem: '#date'
-  });
-  laydate.render({
-    elem: '#date1'
-  });
+    var form = layui.form
+      ,layer = layui.layer
+      ,layedit = layui.layedit
+      ,laydate = layui.laydate;
+    //日期
+    laydate.render({
+        elem: '#date'
+    });
+    laydate.render({
+        elem: '#date1'
+    });
 });
 $('.list').delegate('.list-title>span', 'click',function () {
     // 关闭绘制工具，清除画圈
     if (drawingManager) {
-      drawingManager.close()
+        drawingManager.close()
     }
     if (overlays.length && overlays.length > 0) {
-      for(var i = 0; i < overlays.length; i++){
-        map.removeOverlay(overlays[i]);
-      }
-      overlays.length = 0
-      overlays = []
+        for(var i = 0; i < overlays.length; i++){
+            map.removeOverlay(overlays[i]);
+        }
+        overlays.length = 0
+        overlays = []
     }
     var thisId = $(this).attr('id')
     $(this).parent().addClass('act').siblings().removeClass('act');
@@ -64,34 +64,34 @@ $('.list').delegate('.list-title>span', 'click',function () {
 })
 // 随机生成曲线轨迹
 function createCruvue() {
-  var bounds = map.getBounds();
-  var sw = bounds.getSouthWest();
-  var ne = bounds.getNorthEast();
-  var lngSpan = Math.abs(sw.lng - ne.lng);
-  var latSpan = Math.abs(ne.lat - sw.lat);
+    var bounds = map.getBounds();
+    var sw = bounds.getSouthWest();
+    var ne = bounds.getNorthEast();
+    var lngSpan = Math.abs(sw.lng - ne.lng);
+    var latSpan = Math.abs(ne.lat - sw.lat);
 
-  // map.centerAndZoom(new BMap.Point(106.574737, 29.581328), 16);
-  var pointsArr = []
-  for (var i = 0; i < 8; i ++) {
-    var point = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
-    var marker = new BMap.Marker(point);
-    var label = new BMap.Label(i+1,{offset:new BMap.Size(5, 0)})
-    label.setStyle({
-       border: 'none',
-       background: 'none',
-       color : "#fff",
-			 fontSize : "12px",
-			 height : "20px",
-			 lineHeight : "20px",
-			 fontFamily:"微软雅黑"
-     });
-    map.addOverlay(marker);
-    marker.setLabel(label)
-    pointsArr.push(point);
-  }
-  var curve = new BMapLib.CurveLine(pointsArr, {strokeColor:"blue", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
-  map.addOverlay(curve); //添加到地图中
-  // curve.enableEditing(); //开启编辑功能
+    // map.centerAndZoom(new BMap.Point(106.574737, 29.581328), 16);
+    var pointsArr = []
+    for (var i = 0; i < 8; i ++) {
+        var point = new BMap.Point(sw.lng + lngSpan * (Math.random() * 0.7), ne.lat - latSpan * (Math.random() * 0.7));
+        var marker = new BMap.Marker(point);
+        var label = new BMap.Label(i+1,{offset:new BMap.Size(5, 0)})
+        label.setStyle({
+            border: 'none',
+            background: 'none',
+            color : "#fff",
+            fontSize : "12px",
+            height : "20px",
+            lineHeight : "20px",
+            fontFamily:"微软雅黑"
+        });
+        map.addOverlay(marker);
+        marker.setLabel(label)
+        pointsArr.push(point);
+    }
+    var curve = new BMapLib.CurveLine(pointsArr, {strokeColor:"blue", strokeWeight:3, strokeOpacity:0.5}); //创建弧线对象
+    map.addOverlay(curve); //添加到地图中
+    // curve.enableEditing(); //开启编辑功能
 }
 // 百度地图API功能
 var map = new BMap.Map('map');
@@ -174,10 +174,10 @@ $('.ipt-btn').click(function (e) {
         $.getScript("js/city-picker.js");
         layui.use(['form', 'layedit', 'laydate', 'table'], function(){
             var form = layui.form
-                ,layer = layui.layer
-                ,layedit = layui.layedit
-                ,table = layui.table
-                ,laydate = layui.laydate;
+              ,layer = layui.layer
+              ,layedit = layui.layedit
+              ,table = layui.table
+              ,laydate = layui.laydate;
             //日期
             laydate.render({
                 elem: '#date'
@@ -329,47 +329,47 @@ $("#operate").delegate('#operateBtn span','click',function () {
 var drawingManager;
 var overlays = [];
 function createCircle() {
-  map.enableScrollWheelZoom();
-  //实例化鼠标绘制工具
-  if (!drawingManager) {
-    var overlaycomplete = function(e){
-      // 绘制完成后
-      if (overlays.length !== 0) {
-        clearAll();
-      }
-      overlays.push(e.overlay);
-      console.log('complete');
-    };
-    var styleOptions = {
-      strokeColor:"#A6CBA1",    //边线颜色。
-      fillColor:"#A6CBA1",      //填充颜色。当参数为空时，圆形将没有填充效果。
-      strokeWeight: 3,       //边线的宽度，以像素为单位。
-      strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
-      fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
-      strokeStyle: 'solid' //边线的样式，solid或dashed。
+    map.enableScrollWheelZoom();
+    //实例化鼠标绘制工具
+    if (!drawingManager) {
+        var overlaycomplete = function(e){
+            // 绘制完成后
+            if (overlays.length !== 0) {
+                clearAll();
+            }
+            overlays.push(e.overlay);
+            console.log('complete');
+        };
+        var styleOptions = {
+            strokeColor:"#A6CBA1",    //边线颜色。
+            fillColor:"#A6CBA1",      //填充颜色。当参数为空时，圆形将没有填充效果。
+            strokeWeight: 3,       //边线的宽度，以像素为单位。
+            strokeOpacity: 0.8,	   //边线透明度，取值范围0 - 1。
+            fillOpacity: 0.6,      //填充的透明度，取值范围0 - 1。
+            strokeStyle: 'solid' //边线的样式，solid或dashed。
+        }
+        drawingManager = new BMapLib.DrawingManager(map, {
+            isOpen: true, //是否开启绘制模式
+            enableDrawingTool: false, //是否显示工具栏
+            drawingToolOptions: {
+                anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
+                offset: new BMap.Size(5, 5), //偏离值
+            },
+            circleOptions: styleOptions, //圆的样式
+            polylineOptions: styleOptions, //线的样式
+            polygonOptions: styleOptions, //多边形的样式
+            rectangleOptions: styleOptions //矩形的样式
+        });
+        drawingManager.addEventListener('overlaycomplete', overlaycomplete);
+        function clearAll() {
+            for(var i = 0; i < overlays.length; i++){
+                map.removeOverlay(overlays[i]);
+            }
+            overlays.length = 0
+            overlays = []
+        }
+    } else {
+        drawingManager.open();
     }
-    drawingManager = new BMapLib.DrawingManager(map, {
-      isOpen: true, //是否开启绘制模式
-      enableDrawingTool: false, //是否显示工具栏
-      drawingToolOptions: {
-        anchor: BMAP_ANCHOR_TOP_RIGHT, //位置
-        offset: new BMap.Size(5, 5), //偏离值
-      },
-      circleOptions: styleOptions, //圆的样式
-      polylineOptions: styleOptions, //线的样式
-      polygonOptions: styleOptions, //多边形的样式
-      rectangleOptions: styleOptions //矩形的样式
-    });
-    drawingManager.addEventListener('overlaycomplete', overlaycomplete);
-    function clearAll() {
-      for(var i = 0; i < overlays.length; i++){
-        map.removeOverlay(overlays[i]);
-      }
-      overlays.length = 0
-      overlays = []
-    }
-  } else {
-    drawingManager.open();
-  }
-  drawingManager.setDrawingMode(BMAP_DRAWING_CIRCLE);
+    drawingManager.setDrawingMode(BMAP_DRAWING_CIRCLE);
 }
